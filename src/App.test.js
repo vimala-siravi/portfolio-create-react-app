e7f6eb06df81ef5c72d6f957d982284e0f373e1b
Vimala Sirvi <vimala.sirvi@sungardas.com>
2020-11-21 15:39:31 +0530

    Refactored the existing test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders main page", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Full Stack Web Developer/i);
   expect(linkElement).toBeInTheDocument();
 });
